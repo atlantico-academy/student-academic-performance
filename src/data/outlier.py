b5404939128728a1699ab1c8de191125caf5d70e
Madson Dias <madsonddias@gmail.com>
2022-10-14 16:09:25 -0300

    feat: adiciona remoção de dados discrepantes

--- src/data/outlier.py
+++ src/data/outlier.py
@@ -4,21 +4,24 @@ from scipy.spatial.distance import cdist
 
 
 class OutlierExtractor(TransformerMixin):
-    def __init__(self, threshold=3, **kwargs):
+    def __init__(self, columns, threshold=3, **kwargs):
         self.threshold = threshold
+        self.columns = columns
         self.kwargs = kwargs
 
     def transform(self, X, y=None):
-        X = np.asarray(X)
-        std = np.std(X)
-        outliers_zscore = np.abs(cdist(X.mean(axis=0)[np.newaxis], X) / std)[0]
-        if y:
+        X_ = np.asarray(X[self.columns])
+        outliers_zscore = np.abs(cdist(self.avg[np.newaxis], X_) / self.std)[0]
+        if y is not None:
             y = np.asarray(y)
             return (
-                X[outliers_zscore >= self.threshold, :],
-                y[outliers_zscore >= self.threshold, :]
+                X[outliers_zscore <= self.threshold],
+                y[outliers_zscore <= self.threshold]
             )
-        return X[outliers_zscore >= self.threshold, :]
+        return X[outliers_zscore <= self.threshold]
     
-    def fit(self, *args, **kwargs):
+    def fit(self, X, y=None):
+        X = np.asarray(X[self.columns])
+        self.std = X.std()
+        self.avg = X.mean(axis=0)
         return self
